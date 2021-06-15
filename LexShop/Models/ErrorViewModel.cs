da97cd569ab1568164112e78ad74e07781e934fc
o.kohut <o.kohut@abtollc.com>
2021-06-15 23:50:46 +0300

    Remove cookie consent

--- LexShop/Models/ErrorViewModel.cs
+++ LexShop/Models/ErrorViewModel.cs
@@ -1,4 +1,7 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace LexShop.Models
 {
@@ -6,6 +9,6 @@ namespace LexShop.Models
 	{
 		public string RequestId { get; set; }
 
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+		public bool ShowRequestId => !String.IsNullOrEmpty(RequestId);
 	}
 }
